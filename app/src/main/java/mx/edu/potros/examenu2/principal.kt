ae2fbaabaa5cf9ecb4614639335e8df9ecf8cb6e
amse1029 <alexa.soto236348@potros.itson.edu.mx>
2024-03-13 11:52:40 -0700

    Update principal.kt

--- app/src/main/java/mx/edu/potros/examenu2/principal.kt
+++ app/src/main/java/mx/edu/potros/examenu2/principal.kt
@@ -10,6 +10,8 @@ class principal : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
 
+
+
         val btn_detalles: Button = findViewById(R.id.btn_detalles)
         val btn_regalos: Button = findViewById(R.id.btn_regalos)
         val btn_globos: Button = findViewById(R.id.btn_globos)
@@ -18,26 +20,31 @@ class principal : AppCompatActivity() {
 
         btn_detalles.setOnClickListener() {
             var intent: Intent = Intent(this, regalos::class.java)
+            intent.putExtra("selection", "Detalles")
             startActivity(intent)
         }
 
         btn_regalos.setOnClickListener() {
             var intent: Intent = Intent(this, regalos::class.java)
+            intent.putExtra("selection", "Regalos")
             startActivity(intent)
         }
 
         btn_globos.setOnClickListener() {
             var intent: Intent = Intent(this, regalos::class.java)
+            intent.putExtra("selection", "Globos")
             startActivity(intent)
         }
 
         btn_tazas.setOnClickListener() {
             var intent: Intent = Intent(this, regalos::class.java)
+            intent.putExtra("selection", "Tazas")
             startActivity(intent)
         }
 
         btn_peluches.setOnClickListener() {
             var intent: Intent = Intent(this, regalos::class.java)
+            intent.putExtra("selection", "Peluches")
             startActivity(intent)
         }
     }
